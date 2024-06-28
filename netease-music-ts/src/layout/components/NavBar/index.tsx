5a6a72864fc940ede7278c6badaf7b9afc27ef9d
poison02 <2069820192@qq.com>
2024-06-28 15:23:37 +0800

    feat: 完善路由跳转逻辑

--- netease-music-ts/src/layout/components/NavBar/index.tsx
+++ netease-music-ts/src/layout/components/NavBar/index.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
-import { Avatar, Nav } from "@douyinfe/semi-ui";
+import React, { useMemo, useState } from "react";
+import { Avatar, Nav, Tooltip } from "@douyinfe/semi-ui";
 import { IconMoon, IconPulse, IconSun } from "@douyinfe/semi-icons";
 import "./index.scss";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function NavBar() {
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+	const selectKeys = useMemo(() => [pathname], [pathname]);
 	// 模式切换
 	const [themeMode, setThemeMode] = useState<"dark" | "light">("light");
 	const switchMode = () => {
@@ -21,10 +25,13 @@ function NavBar() {
 			mode={"horizontal"}
 			className="cloud-layout-navbar px-24"
 			items={[
-				{ itemKey: "home", text: "首页" },
-				{ itemKey: "category", text: "分类" }
+				{ itemKey: "/", text: "首页" },
+				{ itemKey: "/category", text: "分类" }
 			]}
-			onSelect={(key) => console.log(key)}
+			selectedKeys={selectKeys}
+			onClick={(item) => {
+				navigate(item.itemKey as string);
+			}}
 			header={{
 				logo: <IconPulse className="text-4xl" size="inherit" />,
 				text: "CloudMusic",
@@ -35,17 +42,21 @@ function NavBar() {
 			footer={
 				<div className="flex items-center">
 					{themeMode === "dark" ? (
-						<IconSun
-							className="text-4xl cursor-pointer"
-							size="inherit"
-							onClick={switchMode}
-						/>
+						<Tooltip content="切换到亮色模式" position="bottom">
+							<IconSun
+								className="text-4xl cursor-pointer"
+								size="inherit"
+								onClick={switchMode}
+							/>
+						</Tooltip>
 					) : (
-						<IconMoon
-							className="text-4xl cursor-pointer"
-							size="inherit"
-							onClick={switchMode}
-						/>
+						<Tooltip content="切换到暗色模式" position="bottom">
+							<IconMoon
+								className="text-4xl cursor-pointer"
+								size="inherit"
+								onClick={switchMode}
+							/>
+						</Tooltip>
 					)}
 
 					<Avatar size="small" className="ml-3" color="red">
